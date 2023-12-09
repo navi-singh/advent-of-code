a9e97d789016116b7ee51b43d80463e387812bac
navi-singh <33882768+navi-singh@users.noreply.github.com>
2023-12-09 13:06:43 -0500

    Add files via upload

---
+++ AOC/day3.py
@@ -0,0 +1,61 @@
+import re
+from collections import namedtuple
+from collections import defaultdict
+
+
+def readInput():
+    with open('/Users/nmehrok/Desktop/python/AOC/Input/day3.txt', 'r') as file:
+        data = file.read().strip()
+    lines = data.split("\n")
+    return lines
+
+
+Range = namedtuple('Range', ['line_number', 'start', 'end'])
+def isSymbol(x): return x != "."
+
+
+def test1(lines):
+    result = 0
+    for idx, line in enumerate(lines):
+        for match in re.finditer(r"\d+", line):
+            foundSymbol = False
+            if (match.start() > 0 and isSymbol(lines[idx][match.start() - 1])) or (match.end() < len(lines[idx]) and isSymbol(lines[idx][match.end()])):
+                foundSymbol = True
+            else:
+                ranges = [Range(idx - 1, match.start() - 1, match.end() + 1)] + \
+                    [Range(idx + 1, match.start() - 1, match.end() + 1)]
+                for r in ranges:
+                    for x in range(r.start, r.end):
+                        if 0 <= r.line_number < len(lines) and 0 <= x < len(lines[r.line_number]) and isSymbol(lines[r.line_number][x]):
+                            foundSymbol = True
+            if foundSymbol:
+                result += int(match.group())
+    return result
+
+
+def test2(lines):
+    result = 0
+    adj = defaultdict(list)
+    for idx, line in enumerate(lines):
+        for match in re.finditer(r"\d+", line):
+            if (match.start() > 0 and isSymbol(lines[idx][match.start() - 1])):
+                adj[idx, match.start()-1].append(match.group())
+            if (match.end() < len(lines[idx]) and isSymbol(lines[idx][match.end()])):
+                adj[idx, match.end()].append(match.group())
+            ranges = [Range(idx - 1, match.start() - 1, match.end() + 1)] + \
+                [Range(idx + 1, match.start() - 1, match.end() + 1)]
+            for current_range in ranges:
+                for index in range(current_range.start, current_range.end):
+                    if 0 <= current_range.line_number < len(lines) and 0 <= index < len(lines[current_range.line_number]) and isSymbol(lines[current_range.line_number][index]):
+                        adj[current_range.line_number,
+                            index].append(match.group())
+
+    for match_gears in adj.values():
+        if len(match_gears) == 2:
+            result += int(match_gears[0]) * int(match_gears[1])
+    return result
+
+
+lines = readInput()
+print(test1(lines))
+print(test2(lines))
---
+++ AOC/day9.py
@@ -0,0 +1,60 @@
+import math
+
+
+def readInput():
+    with open('/Users/nmehrok/Desktop/python/AOC/Input/day9.txt', 'r') as file:
+        data = file.read().strip()
+    lines = data.split("\n")
+    return lines
+
+
+def generate_list(line):
+    input = []
+    input.append([int(value) for value in line.split()])
+    for i in range(0, len(input[0])):
+        list = []
+        for j in range(1, len(input[i])):
+            list.append(input[i][j] - input[i][j-1])
+        if all(elem == 0 for elem in list):
+            break
+        input.append(list)
+    return input
+
+
+def get_next(grid):
+    result = 0
+    for i in range(len(grid)-1, -1, -1):
+        last_index = len(grid[i])-1
+        result += grid[i][last_index]
+    return result
+
+
+def get_previous(grid):
+    res = sum(row[0] for row in reversed(grid))
+    result = 0
+    for i in range(len(grid)-1, -1, -1):
+        result = grid[i][0] - result
+    return result
+
+
+def part1(lines):
+    result = 0
+    for x in lines:
+        grid = generate_list(x)
+        next = get_next(grid)
+        result += next
+    return result
+
+
+def part2(lines):
+    result = 0
+    for x in lines:
+        grid = generate_list(x)
+        next = get_previous(grid)
+        result += next
+    return result
+
+
+lines = readInput()
+print(part1(lines))
+print(part2(lines))
